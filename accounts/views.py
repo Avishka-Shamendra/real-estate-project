0789739b14ae422fe3cf0e9afb1190cb133421b1
Avishka-Shamendra <avishka199849@gmail.com>
2021-05-18 20:42:53 +0530

    login and reg templates added

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render,redirect
 
 def register(request):
+    if(request.method=='POST'):
+        #  Register user
+        return redirect('register')
     return render(request,'accounts/register.html')
 
 def login(request):
+    if(request.method=='POST'):
+        # Login user
+        return redirect('Login')
     return render(request,'accounts/login.html')
 
 def logout(request):

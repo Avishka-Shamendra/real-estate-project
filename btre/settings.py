10bc12040a35dd078850da390935c1eef7a58226
Avishka-Shamendra <avishka199849@gmail.com>
2021-05-23 22:24:52 +0530

    local settings

--- btre/settings.py
+++ btre/settings.py
@@ -155,3 +155,8 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_USER ='example@gmail.com'
 # EMAIL_HOST_PASSWORD ='example'
 # EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
